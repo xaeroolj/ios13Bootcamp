3a304599f49d65926c48fad96ee8ec4b044e523d
Roman Trekhlebov <xaero_olj@mail.ru>
2020-03-27 00:17:10 +0300

    randomize dice, fix crash code refactoring

--- Dicee/Dicee-iOS13/ViewController.swift
+++ Dicee/Dicee-iOS13/ViewController.swift
@@ -13,24 +13,13 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
-    
-    var leftDiceNumber = 1
-    var rightDiceNumber = 5
-    let diceImageArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
 
-        
-    }
+    let diceImageArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
 
     @IBAction func rollButtonWasPressed(_ sender: Any) {
-        diceImageView1.image =  diceImageArray[leftDiceNumber]
-        diceImageView2.image =  diceImageArray[rightDiceNumber]
-        
-        leftDiceNumber += 1
-        rightDiceNumber -= 1
+        diceImageView1.image =  diceImageArray.randomElement()
+        diceImageView2.image =  diceImageArray[Int.random(in: 0...5)]
+
     }
     
 }
