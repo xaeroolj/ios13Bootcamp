8b08e0079677ee2a1f023fc135f7fff31813439e
Roman Trekhlebov <xaero_olj@mail.ru>
2020-03-29 08:07:11 +0300

    Implementing 2 different functions for playing sound

--- Xylophone/Xylophone/ViewController.swift
+++ Xylophone/Xylophone/ViewController.swift
@@ -7,9 +7,41 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
 
+    
+    var playerBig: AVAudioPlayer?
+    var player: AVAudioPlayer!
+
+    //More functionality (can play sound ion silent mode)
+    func playSoundBig() {
+        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
+            
+            guard let player = player else { return }
+            player.play()
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    //will not work in silent mode
+    func playSound() {
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -17,7 +49,7 @@ class ViewController: UIViewController {
 
     
     @IBAction func keyPressed(_ sender: UIButton) {
-        print("Red C button was pressed!")
+        playSound()
     }
     
 }
