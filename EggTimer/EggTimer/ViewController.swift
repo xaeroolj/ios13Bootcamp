1db6f39f7fd518fc204bf9a466b91e70b8cd1723
Roman Trekhlebov <xaero_olj@mail.ru>
2020-04-05 14:25:28 +0300

    Change ui, Create constants for hardness time, link buttons to IBAction

--- EggTimer/EggTimer/ViewController.swift
+++ EggTimer/EggTimer/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
 
 
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+    }
 }
