24064480d0ee5c247c5420c84c34dd117ba41374
Roman Trekhlebov <xaero_olj@mail.ru>
2020-04-05 17:06:17 +0300

    Implement countdown timer and changing UI, restart..

--- EggTimer/EggTimer/ViewController.swift
+++ EggTimer/EggTimer/ViewController.swift
@@ -10,23 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var mainLbl: UILabel!
+    
     let eggTimes = ["Soft" : 5,
                     "Medium" : 7,
                     "Hard" : 12]
+    var timer = Timer()
 
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        mainLbl.text = "How do you like your eggs?"
         let hardness = sender.currentTitle!
         
 //        print(eggTimes[hardness]!)
+        timer.invalidate()
+//        countdownStart(eggTimes[hardness]!)
         countdownStart(eggTimes[hardness]! * 60)
+
         
     }
     
     func countdownStart(_ seconds: Int) {
         var secondsLeft = seconds
-        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (timer) in
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (timer) in
             if secondsLeft == 0 {
+                self.mainLbl.text = "Done"
                 print(secondsLeft)
                 timer.invalidate()
             }else {
