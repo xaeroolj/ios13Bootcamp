963aea486d887a5545129cd5f6ce042d6a4b3239
Roman Trekhlebov <xaero_olj@mail.ru>
2020-04-05 13:48:24 +0300

    Use currentTitle for play correct sound for Buttons

--- Xylophone/Xylophone/ViewController.swift
+++ Xylophone/Xylophone/ViewController.swift
@@ -34,8 +34,8 @@ class ViewController: UIViewController {
     }
     
     //will not work in silent mode
-    func playSound() {
-        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+    func playSound(_ soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
         player = try! AVAudioPlayer(contentsOf: url!)
         player.play()
                 
@@ -50,7 +50,7 @@ class ViewController: UIViewController {
     
     @IBAction func keyPressed(_ sender: UIButton) {
         print(sender.currentTitle!)
-        playSound()
+        playSound(sender.currentTitle!)
     }
     
 }
