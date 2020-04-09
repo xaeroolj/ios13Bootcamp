5cc1fee10a2b3797c3870d184c68a3c5d0ac811e
Roman Trekhlebov <xaero_olj@mail.ru>
2020-04-09 17:34:07 +0300

    Iboutlets to weight and height labels. connect slider to labels

--- BMI-Calculator/BMI Calculator/Controllers/ViewController.swift
+++ BMI-Calculator/BMI Calculator/Controllers/ViewController.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //IBOutlets
+    
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -25,14 +30,16 @@ class ViewController: UIViewController {
         let roundedValue = formatter.string(from: NSNumber(value: slider.value))
 
         
-        print(roundedValue!)
+//        print(roundedValue!)
+        heightLbl.text = "\(roundedValue!)m"
     }
     
     @IBAction func weightSliderDidChanged(_ sender: UISlider) {
         
         
 //        print(String(format: "%.0f", sender.value))
-        print(Int(sender.value))
+//        print(Int(sender.value))
+        weightLbl.text = "\(Int(sender.value))kg"
     }
     
 
