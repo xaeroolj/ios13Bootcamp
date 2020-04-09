b0bf41503ca492dd90cc961f4d195488f5529833
Roman Trekhlebov <xaero_olj@mail.ru>
2020-04-09 15:56:02 +0300

    Step 5: Apply MVC (Implement storyBrain ) clear controller for brain class

--- Destini/Destini-iOS13/Controller/ViewController.swift
+++ Destini/Destini-iOS13/Controller/ViewController.swift
@@ -14,19 +14,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
-    //Constants:
+    var storyBrain = StoryBrain()
     
-    var storyLineNumber = 0
-    
-    let example = Story(title: "You see a fork in the road.",
-                        choice1: "Take a left.",
-                        choice2: "Take a right.")
-    let storyArray = [
-        Story(title: "You see a fork in the road.", choice1: "Take a left.",
-        choice2: "Take a right."),
-        Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
-        Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
-    ]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,27 +25,17 @@ class ViewController: UIViewController {
     }
 
     @IBAction func choiceMade(_ sender: UIButton) {
-        //Optional line
-        guard storyLineNumber == 0 else {return}
+        guard let text = sender.titleLabel?.text else { return }
+        storyBrain.nextStory(text)
         
-        switch sender.tag {
-        case 0:
-            //choice 1
-            storyLineNumber = 1
-        case 1:
-            //choice 2
-            storyLineNumber = 2
-        default:
-            fatalError("Button tag is wrong.")
-        }
         updateUI()
     }
     
     func updateUI() {
-        
-        storyLabel.text = storyArray[storyLineNumber].title
-        choice1Button.setTitle(storyArray[storyLineNumber].choice1, for: .normal)
-        choice2Button.setTitle(storyArray[storyLineNumber].choice2, for: .normal)
+        let currentStory = storyBrain.storyNumber
+        storyLabel.text = storyBrain.stories[currentStory].title
+        choice1Button.setTitle(storyBrain.stories[currentStory].choice1, for: .normal)
+        choice2Button.setTitle(storyBrain.stories[currentStory].choice2, for: .normal)
     }
     
 }
--- Destini/Destini-iOS13/Model/StoryBrain.swift
+++ Destini/Destini-iOS13/Model/StoryBrain.swift
@@ -8,4 +8,31 @@
 
 import Foundation
 
+struct StoryBrain {
+    var storyNumber = 0
+    
+    let stories = [
+        Story(title: "You see a fork in the road.", choice1: "Take a left.",
+        choice2: "Take a right."),
+        Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
+        Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
+    ]
+    
+    mutating func nextStory(_ userChoice:String) {
+        //Optional line
+        guard storyNumber == 0 else {return}
+        
+        switch userChoice {
+        case "Take a left.":
+            //choice 1
+            storyNumber = 1
+        case "Take a right.":
+            //choice 2
+            storyNumber = 2
+        default:
+            fatalError("Wrong choice.")
+        }
+    }
+    
+}
 
