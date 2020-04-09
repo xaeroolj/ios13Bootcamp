f2446c0092db0dee16760c6a94947adf10ca376a
Roman Trekhlebov <xaero_olj@mail.ru>
2020-04-09 15:40:42 +0300

    Step 4, Link Buttons to Ib action, Implement var for track Storyes, updateUI Func

--- Destini/Destini-iOS13/Controller/ViewController.swift
+++ Destini/Destini-iOS13/Controller/ViewController.swift
@@ -16,28 +16,48 @@ class ViewController: UIViewController {
     
     //Constants:
     
+    var storyLineNumber = 0
+    
     let example = Story(title: "You see a fork in the road.",
                         choice1: "Take a left.",
                         choice2: "Take a right.")
-    let exampleArray = [
+    let storyArray = [
         Story(title: "You see a fork in the road.", choice1: "Take a left.",
         choice2: "Take a right."),
         Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
         Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
     ]
 
-
-
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        storyLabel.text = example.title
-        choice1Button.setTitle(example.choice1, for: .normal)
-        choice2Button.setTitle(example.choice2, for: .normal)
-
+        updateUI()
+        
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {
+        //Optional line
+        guard storyLineNumber == 0 else {return}
+        
+        switch sender.tag {
+        case 0:
+            //choice 1
+            storyLineNumber = 1
+        case 1:
+            //choice 2
+            storyLineNumber = 2
+        default:
+            fatalError("Button tag is wrong.")
+        }
+        updateUI()
+    }
+    
+    func updateUI() {
+        
+        storyLabel.text = storyArray[storyLineNumber].title
+        choice1Button.setTitle(storyArray[storyLineNumber].choice1, for: .normal)
+        choice2Button.setTitle(storyArray[storyLineNumber].choice2, for: .normal)
+    }
+    
 }
 
