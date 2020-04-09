836cdaa57d216613f56beec4dd1686611ecf4f53
Roman Trekhlebov <xaero_olj@mail.ru>
2020-04-09 15:22:34 +0300

    Step 3, Create Story struct, pack data in simple array.

--- Destini/Destini-iOS13/Controller/ViewController.swift
+++ Destini/Destini-iOS13/Controller/ViewController.swift
@@ -15,18 +15,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice2Button: UIButton!
     
     //Constants:
-    let story0 = "You see a fork in the road."
-    let choice1 = "Take a left."
-    let choice2 = "Take a right."
+    
+    let example = Story(title: "You see a fork in the road.",
+                        choice1: "Take a left.",
+                        choice2: "Take a right.")
+    let exampleArray = [
+        Story(title: "You see a fork in the road.", choice1: "Take a left.",
+        choice2: "Take a right."),
+        Story(title: "You see a tiger.", choice1: "Shout for help.", choice2: "Play dead."),
+        Story(title: "You find a treasure chest.", choice1: "Open it.", choice2: "Check for traps.")
+    ]
+
 
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        storyLabel.text = story0
-        choice1Button.setTitle(choice1, for: .normal)
-        choice2Button.setTitle(choice2, for: .normal)
+        storyLabel.text = example.title
+        choice1Button.setTitle(example.choice1, for: .normal)
+        choice2Button.setTitle(example.choice2, for: .normal)
 
     }
 
