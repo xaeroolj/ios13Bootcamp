2ab6eedfa58ecb89842ca0c09b25af5ee5e50dbe
Roman Trekhlebov <xaero_olj@mail.ru>
2020-04-13 23:33:35 +0300

    Create UI from code.

--- BMI-Calculator/BMI Calculator/Controllers/SecondViewController.swift
+++ BMI-Calculator/BMI Calculator/Controllers/SecondViewController.swift
@@ -12,10 +12,18 @@ class SecondViewController: UIViewController {
     
     @IBOutlet weak var resultLbl: UILabel!
     
+    var bmiValue = "0.0"
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.backgroundColor = UIColor.red
+        
+        let label = UILabel()
+        label.text = bmiValue
+        label.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        view.addSubview(label)
     }
     
 }
