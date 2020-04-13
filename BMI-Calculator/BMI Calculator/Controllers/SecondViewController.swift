554c6b1eedf5e121588a1689f3d0eb26e5c85f85
Roman Trekhlebov <xaero_olj@mail.ru>
2020-04-13 22:48:04 +0300

    Start managing second VC

---
+++ BMI-Calculator/BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Roman Trekhlebov on 13.04.2020.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var resultLbl: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
