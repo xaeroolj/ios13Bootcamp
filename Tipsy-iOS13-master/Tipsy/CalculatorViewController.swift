af655c087a2c03ab84a2b0d457bc2f758bd3293d
Roman Trekhlebov <xaero_olj@mail.ru>
2020-04-17 22:09:30 +0300

    Link Storyboard to Ibactions and IBOutlets, Refactor- rename to CalculatorViewController

---
+++ Tipsy-iOS13-master/Tipsy/CalculatorViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    
+    
+    //Outlets
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    
+    
+    //Actions
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+    
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+    
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
+    
+
+
+}
+
