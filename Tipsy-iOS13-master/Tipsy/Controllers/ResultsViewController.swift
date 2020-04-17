2ef23a5f7e9586023b871fb1ce00ba8c8a221c81
Roman Trekhlebov <xaero_olj@mail.ru>
2020-04-17 22:14:29 +0300

    ResultsViewController Linking Outlets and actions to storyboard

---
+++ Tipsy-iOS13-master/Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Roman Trekhlebov on 17.04.2020.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+    
+    //Iboutlets
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    //ibActions
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+    
+}
