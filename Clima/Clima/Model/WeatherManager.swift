f9c35aa865c64b70e78d6619416c37e4df2c7935
Roman Trekhlebov <xaero_olj@mail.ru>
2020-04-25 23:25:24 +0300

    Perform request using URLSEssion

--- Clima/Clima/Model/WeatherManager.swift
+++ Clima/Clima/Model/WeatherManager.swift
@@ -15,5 +15,26 @@ struct WeatherManager {
         let urlString = "\(weatherURl)&q=\(cityName)"
         
         print(urlString)
+        self.performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url, completionHandler: handle(data:responce:error:))
+            task.resume()
+        }
+    }
+    
+    func handle(data: Data?, responce: URLResponse?, error: Error? ) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
     }
 }
