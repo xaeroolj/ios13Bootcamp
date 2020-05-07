6c3fe817c5c4665cfc791c603f9c4de84f592dbe
Roman Trekhlebov <xaero_olj@mail.ru>
2020-05-07 23:30:36 +0300

    Const file

---
+++ Clima/Clima/Model/Constant.swift
@@ -0,0 +1,11 @@
+//
+//  Constant.swift
+//  Clima
+//
+//  Created by Roman Trekhlebov on 25.04.2020.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+let API_KEY = "0e176296533a32896ad01af826ec8d60"
