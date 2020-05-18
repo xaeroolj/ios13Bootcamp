0fb99b04276fb84db4281a0b16eb59089ccc2954
Roman Trekhlebov <xaero_olj@mail.ru>
2020-05-18 16:30:59 +0300

    Perform request to coinapi.io

--- ByteCoin/ByteCoin/Model/CoinManager.swift
+++ ByteCoin/ByteCoin/Model/CoinManager.swift
@@ -16,7 +16,22 @@ struct CoinManager {
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
     func getCoinPrice(for currency: String) {
-        
+        let urlString = "\(baseURL)/\(currency)/?apikey=\(apiKey)"
+        performRequest(with: urlString)
+    }
+    
+    func performRequest(with urlString: String) {
+        guard let url = URL(string: urlString) else {return}
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: url) { (data, responce, error) in
+            if error != nil {
+                print("Have error on request")
+            } else {
+                let responceString = String(data: data!, encoding: .utf8)
+                print(responceString!)
+            }
+        }
+        task.resume()
     }
     
 }
