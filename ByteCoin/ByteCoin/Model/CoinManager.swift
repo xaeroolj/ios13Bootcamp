6dc511f9529a5612f3d5b0fdd1a0549ed5773974
Roman Trekhlebov <xaero_olj@mail.ru>
2020-05-18 16:16:49 +0300

    gitignore for apiKeys

--- ByteCoin/ByteCoin/Model/CoinManager.swift
+++ ByteCoin/ByteCoin/Model/CoinManager.swift
@@ -11,7 +11,7 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = API_KEY
     
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
 
