caab6ebdc9f1216aa70a4b775f82e964ad315d7c
Roman Trekhlebov <xaero_olj@mail.ru>
2020-05-20 06:57:40 +0300

    Text lbl animation using timer and loops

--- Flash-Chat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash-Chat/Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -14,9 +14,20 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        animateLbl()
 
        
     }
-    
+    func animateLbl() {
+        titleLabel.text = ""
+        var charIndex = 0.0
+        let titleText = "⚡️FlashChat"
+        for letter in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
+    }
 
 }
