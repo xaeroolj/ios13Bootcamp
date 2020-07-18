3b339b9d8997d48426971c6f0dbf6127540a4cfc
Roman Trekhlebov <xaero_olj@mail.ru>
2020-07-18 18:20:56 +0300

    Adding model for Message, populate temp data for Messages array, implement tabble view DataSource protocol

--- Flash-Chat/Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash-Chat/Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,10 +14,17 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Message] = [
+        Message(sender: "1@2.com", body: "Hey!"),
+        Message(sender: "2@2.com", body: "Hello!"),
+        Message(sender: "1@2.com", body: "What's up?"),
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = K.appName
         navigationItem.hidesBackButton = true
+        tableView.dataSource = self
 
     }
     
@@ -35,3 +42,20 @@ class ChatViewController: UIViewController {
     }
     
 }
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
+        cell.textLabel?.text = messages[indexPath.row].sender
+        cell.detailTextLabel?.text = messages[indexPath.row].body
+        return cell
+    }
+    
+    
+}
+
+
