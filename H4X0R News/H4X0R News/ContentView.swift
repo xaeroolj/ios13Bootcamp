3276185e396972bbc5d1eb0f3b38f20df750ea83
Roman Trekhlebov <xaero_olj@mail.ru>
2020-07-31 05:03:22 +0300

    Init design structure

--- H4X0R News/H4X0R News/ContentView.swift
+++ H4X0R News/H4X0R News/ContentView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!").padding()
+        NavigationView{
+            List(posts) {post in
+                Text(post.title)
+            }
+            .navigationBarTitle("H4X0R NEWS")
+        }
     }
 }
 
@@ -18,3 +23,15 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+struct Post: Identifiable {
+    let id:String
+    let title: String
+}
+
+let posts = [
+    Post(id: "1", title: "fdsfdssfds"),
+    Post(id: "2", title: "dsfsfsdf"),
+    Post(id: "3", title: "dfsf")
+]
