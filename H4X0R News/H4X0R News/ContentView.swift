1edbf139866824a286bf948fc9a8623016c8a8ec
Roman Trekhlebov <xaero_olj@mail.ru>
2020-07-31 04:55:32 +0300

    H4X0R News init

---
+++ H4X0R News/H4X0R News/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by Roman Trekhlebov on 31.07.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
