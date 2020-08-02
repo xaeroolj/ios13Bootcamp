7f178c98704238e1d517d132eb8a69a886eec8df
Roman Trekhlebov <xaero_olj@mail.ru>
2020-08-02 23:14:20 +0300

    Implement UITableViewController

---
+++ Todoey/Todoey/ToDoListViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ToDoListViewController.swift
+//  Todoey
+//
+//  Created by Philipp Muellauer on 02/12/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+    
+    let itemArray = ["String one", "String Two", "String Three"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    ///ToDoItemCell
+    
+    //MARK - Tableview Datasource Methods
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+}
+
